9b8b8c3bbf2e51836953884f58f74ad3b7116779
Niz <nizniz187@gmail.com>
2020-03-11 23:26:47 +0800

    Initialize environment settings for Vue.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: `${srcPath}/index`,
+  output: {
+    filename: '[name].bundle.js',
+    path: distPath,
+    publicPath: distPath
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    },
+    extensions: ['*', '.js', '.vue']
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname),
+    index: 'index.html',
+    writeToDisk: true,
+    hot: true,
+    watchContentBase: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+            { loader: MiniCssExtractPlugin.loader },
+            'css-loader'
+        ]
+      }
+    ]
+  }
+};
